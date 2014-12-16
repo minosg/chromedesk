209175fb2de495a7f66f5167022bc23c91efdc78
Minos Galanakis <minos197@gmail.com>
2014-12-16 13:23:46 +0000

    Added embedded threaded timer, that can be assigned  a callback Added support for multiplatform wallpaper switching Separated change function in two secondary simple ones.

--- chromeDesk.py
+++ chromeDesk.py
@@ -1,4 +1,4 @@
-import urllib2,ctypes,os,random,threading
+import urllib2,ctypes,os,random,threading,platform
 
 class ChromeDesk():
   def __init__( self , t_rotation = 300, dl_dir = 'Wallpapers'):
@@ -10,6 +10,7 @@ class ChromeDesk():
     self.imgp        = 0
     self.choice      = ""
     self.cleanup_flg = False
+    self.platform    = ( platform.system(), os.getenv("DESKTOP_SESSION") )
 
   def get_source( self ):
     return  urllib2.urlopen('https://clients3.google.com/cast/chromecast/home').read()
@@ -47,6 +48,40 @@ class ChromeDesk():
     self.download_img(output)
     self.image_links = output
     return output
+
+  def stop_periodic_callback( self ):
+    global t1_run_flag
+    t1_run_flag = False
+
+  def attach_periodic_callback ( self, cb ):
+    global t1_run_flag
+    global callback
+    global t_cycle
+
+    #set the configuration
+    t1_run_flag = True
+    callback    = cb
+    t_cycle = self.t_rotation
+
+      #Add background processes
+    def background_timer():
+      import time
+      global t1_run_flag
+      global callback
+      global t_cycle
+      ref_time = time.time()
+      while ( t1_run_flag ):
+        cur_time = time.time()
+        if cur_time - ref_time >= t_cycle:
+          ref_time = cur_time
+          callback()
+        if not t1_run_flag: return
+        time.sleep(0.5)
+    #covert to a thread and start it
+    thread = threading.Thread(target=background_timer)
+    thread.daemon = True
+    thread.start()
+
 
   def download_img ( self, img_dict ):
     output = {}
@@ -86,8 +121,14 @@ class ChromeDesk():
         print "Error processing image type: ",img_name
         continue
 
+      #Append author
       idx = fname.index('.')
       fname = fname[:idx] + "_by_" + author + fname[idx:]
+
+      #replace chars to make the naming more readable
+      fname = fname.replace(" ","_")
+      fname = fname.replace("%","_")
+
       fname = os.path.join( self.dl_dir ,fname)
       with open(fname,'wb') as f:
         f.write(output[author])
@@ -133,21 +174,39 @@ class ChromeDesk():
 
     if old_file and self.choice:
       self.cleanup( old_file )
-      return os.path.join( self.dl_dir, self.choice )
+    return os.path.join( self.dl_dir, self.choice )
 
-  def change( self ):
-
+  def next( self ):
     #If the images have not been downloaded yet
     if not ( self.image_links ):
       self.image_links = self.extract_img( self.get_source() )
 
-    SPI_SETDESKWALLPAPER = 20 
-
+    #Select the next image file
     rimage = self.image_picker()
+    self.change( rimage )
 
-    #Set it as wallpaper
-    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, rimage , 0)
+  def change( self, rimage ):
+    print "Changing Wallpaper"
+    if self.platform[0] == "Windows":
+      SPI_SETDESKWALLPAPER = 20 
+
+      #Set it as wallpaper
+      ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, rimage , 0)
+    elif self.platform[0] == "Linux":
+      window_manager = self.platform[1]
+      rimage = os.path.abspath(rimage)
+      if 'gnome' in window_manager:
+        command = 'gconftool-2 -t string -s /desktop/gnome/background/picture_filename %s' % rimage
+      elif 'kde' in window_manager:
+        command = 'dcop kdesktop KBackgroundIface setWallpaper %s 1' % rimage
+      elif "ubuntu" in window_manager:
+        command = "gsettings set org.gnome.desktop.background picture-uri file:///%s" % rimage
+      os.system(command)
+      else:
+        print "Unrecognised Desktop Environment %s"%window_manager
+    else:
+      print "Unrecognised platform %s"%self.platform[0]
 
 if __name__ == '__main__':
   cd = ChromeDesk()
-  cd.change()
+  cd.next()
