db44fe95f6781fffd03528bcc977fcbcc207276f
Minos Galanakis <minos197@gmail.com>
2014-12-17 12:44:46 +0000

    Updated threads implementation to utilize callbacks, added an empty folder detection flag

--- chromeDesk.py
+++ chromeDesk.py
@@ -18,14 +18,15 @@ from functools import partial
 class ChromeDesk():
 
   def __init__( self , t_rotation = 300, dl_dir = 'Wallpapers'):
-    self.dl_dir      = dl_dir
-    self.t_rotation  = t_rotation
-    self.image_links = None
-    self.running     = False
-    self.imgp        = 0
-    self.choice      = ""
-    self.cleanup_flg = False
-    self.platform    = ( platform.system(), os.getenv("DESKTOP_SESSION") )
+    self.dl_dir       = dl_dir
+    self.t_rotation   = t_rotation
+    self.image_links  = None
+    self.folder_empty = True
+    self.running      = False
+    self.imgp         = 0
+    self.choice       = ""
+    self.cleanup_flg  = False
+    self.platform     = ( platform.system(), os.getenv("DESKTOP_SESSION") )
 
   def get_source( self ):
     return  urllib2.urlopen('https://clients3.google.com/cast/chromecast/home').read()
@@ -70,20 +71,15 @@ class ChromeDesk():
 
   def attach_periodic_callback ( self, cb ):
     global t1_run_flag
-    global callback
-    global t_cycle
 
     #set the configuration
     t1_run_flag = True
-    callback    = cb
-    t_cycle = self.t_rotation
 
     #Add background processes
-    def background_timer():
+    def background_timer( t_cycle, callback ):
       import time
       global t1_run_flag
-      global callback
-      global t_cycle
+
       ref_time = time.time()
       while ( t1_run_flag ):
         cur_time = time.time()
@@ -93,21 +89,17 @@ class ChromeDesk():
         if not t1_run_flag: return
         time.sleep(0.5)
     #covert to a thread and start it
-    thread = threading.Thread(target=background_timer)
+    thread = threading.Thread(target=background_timer,args = (self.t_rotation,cb,))
     thread.daemon = True
     thread.start()
 
 
   def download_img ( self, img_dict ):
-    global input_dict
-
-    #attach a callback to change wallpaper after the first image downloaded
-    cb = self.next
-    input_dict = img_dict
+    def remove_empty():
+      self.folder_empty = False
 
     #/Start of thread
-    def background_downloader( cb ):
-      global input_dict
+    def background_downloader( change_cb, empty_cb ):
       output = {}
       first_img = True
       #Check if download directory exits and make it if not
@@ -162,18 +154,30 @@ class ChromeDesk():
 
         with open(fname,'wb') as f:
           f.write(output[author])
+
         f.close()
         #call the callback once
         if first_img:
-          cb()
+          #change the wallpaper to the file just downloaded
+          ( lambda: change_cb( fname ) )()
+          #Notify the system the folder is no longer empty
+          empty_cb()
           first_img = False
-
         #/End of thread
 
     #covert to a thread and start it
-    thread = threading.Thread(target=background_downloader,args = (cb,))
+    thread = threading.Thread(target=background_downloader,args = (self.change,remove_empty,))
     thread.daemon = True
     thread.start()
+
+  def set_download_dir( self, dir ):
+    self.dl_dir = dir
+    #Check if download directory exits and make it if not
+    if not os.path.exists( self.dl_dir ):
+      os.makedirs( self.dl_dir )
+
+    #reset the links database to force the program to dl them in new location
+    self.image_links = ''
 
   def set_image_cleanup ( self,mode ):
     self.cleanup_flg = mode
@@ -190,7 +194,9 @@ class ChromeDesk():
 
     #if no images left in directory download new ones
     if not os.listdir( self.dl_dir ):
-      self.image_links = self.extract_img( self.get_source() )
+      self.folder_empty = True
+      #self.image_links = self.extract_img( self.get_source() )
+
 
   def image_picker ( self ):
     old_file = ''
@@ -202,7 +208,11 @@ class ChromeDesk():
     wallpapers = os.listdir( self.dl_dir )
     if self.imgp == 0:
       #Select one at random
-      self.choice = random.choice( wallpapers )
+      while True:
+        self.choice = random.choice( wallpapers )
+        #make sure the same file is not chosen unless its is the only one
+        if self.choice not in old_file or len(wallpapers) == 1:
+          break
 
     elif self.imgp == 1:
       try:
@@ -210,23 +220,23 @@ class ChromeDesk():
       #If the first time it runs or index overflow
       except (IndexError,ValueError):
         self.choice = wallpapers[0]
-      #Compose the path
-
+    #Compose the path
     if old_file and self.choice:
       self.cleanup( old_file )
     return os.path.join( self.dl_dir, self.choice )
 
   def next( self ):
     #If the images have not been downloaded yet
-    if not ( self.image_links ):
+    if ( self.folder_empty ):
       self.image_links = self.extract_img( self.get_source() )
+      self.choice = ''
       return 
     #Select the next image file
     rimage = self.image_picker()
     self.change( rimage )
 
   def change( self, rimage ):
-    print "Changing Wallpaper"
+    #print "Changing Wallpaper"
     if self.platform[0] == "Windows":
       SPI_SETDESKWALLPAPER = 20 
 
